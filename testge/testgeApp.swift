b2d1595a970d13fb0934ed667edb4319cc9e3970
Dominik Hofer <dhofer@esri.com>
2024-04-26 14:01:22 +0200

    Initial Commit

---
+++ testge/testgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgeApp.swift
+//  testge
+//
+//  Created by Dominik Hofer on 26.04.24.
+//
+
+import SwiftUI
+
+@main
+struct testgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
